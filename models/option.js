c752cc2ba52bb3d34ba907d2bdc6c11a50cf4a05
Simon Sun <blackghost0118@gmail.com>
2016-03-02 10:16:52 +0800

    add model static method findBySn

--- models/option.js
+++ models/option.js
@@ -1,10 +1,18 @@
 
+import co from 'co';
+import Promise from 'bluebird';
+import is from 'is_js';
 import mongoose from 'mongoose';
 import autoIncrement from 'mongoose-auto-increment';
 
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
+
+    sn: {
+        type: Number,
+        required: true
+    },
 
     content: {
         type: String,
@@ -25,6 +33,21 @@ const schema = new Schema({
         type: Date,
         default: Date.now
     }
+});
+
+schema.statics.findBySn = co.wrap(function*(sn) {
+
+    let self = this;
+    let thisSn = parseInt(sn, 10);
+
+    if(!is.number(thisSn)){
+        return yield Promise.reject(new Error('sn must number'));
+    }
+
+    return yield self
+        .findOne()
+        .where('sn').equals(thisSn)
+        .execAsync();
 });
 
 schema.plugin(autoIncrement.plugin, {
