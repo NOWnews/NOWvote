77941dde4b7470cdfc9e2df12349900a9950e81d
webber0928 <bird23074035@gmail.com>
2016-03-07 10:38:16 +0800

    menu 流程基本建立

--- admin/controllers/menu/index.js
+++ admin/controllers/menu/index.js
@@ -2,8 +2,26 @@ import express from 'express';
 let router = express.Router();
 
 import pageMenu from './page.menu';
+import pageCreate from './page.create';
+// import pageEdit from './page.edit';
+// import pageDelete from './page.delete';
 
 router.route('/')
     .get(pageMenu);
+
+router.route('/create')
+    .get(pageCreate.show)
+    .post(pageCreate.create);
+//
+// router.route('/edit/:id')
+//     .put(pageEdit.put)
+//     .get(pageEdit.get);
+//
+// router.route('/sortable')
+//     .post(pageSortable);
+//
+// router.route('/delete/:ids')
+//     .delete(pageDelete);
+
 
 module.exports = router;
