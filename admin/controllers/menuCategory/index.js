2a5aa04c296686fbaa42aff38f336cf81e9d7541
webber0928 <bird23074035@gmail.com>
2016-03-08 01:18:26 +0800

    加上 刪除以及刪除的確認視窗

--- admin/controllers/menuCategory/index.js
+++ admin/controllers/menuCategory/index.js
@@ -3,6 +3,7 @@ let router = express.Router();
 
 import pageList from './page.list';
 import activeUpdateList from './active.updateList';
+import activeRemove from './active.remove';
 import pageCreate from './page.create';
 // import pageEdit from './page.edit';
 // import pageDelete from './page.delete';
@@ -11,9 +12,13 @@ router.route('/')
     .get(pageList)
     .put(activeUpdateList);
 
-router.route('/create')
-    .get(pageCreate.show)
-    .post(pageCreate.create);
+router.route('/:id')
+    .delete(activeRemove);
+
+
+// router.route('/create')
+//     .get(pageCreate.show)
+//     .post(pageCreate.create);
 //
 // router.route('/edit/:id')
 //     .put(pageEdit.put)
