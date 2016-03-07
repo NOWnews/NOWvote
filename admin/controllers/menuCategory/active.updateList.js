198c187a7b248df35635d5a2836f398601aa1ccb
webber0928 <bird23074035@gmail.com>
2016-03-08 01:47:38 +0800

    沒辦法save, 再請教 simon

--- admin/controllers/menuCategory/active.updateList.js
+++ admin/controllers/menuCategory/active.updateList.js
@@ -7,8 +7,21 @@ import models from '../../../models';
 module.exports = function(req, res, next) {
 
     co(function*() {
-        console.log('安安安安', req.body);
-        return res.json(req.body);
+        let data = req.body;
+        let weightList = req.body.weightList.split(',');
+        let statusList = req.body['status[]'];
+        let menuCategorys = yield models.menuCategory.update({ trashed: false }).execAsync();
+        menuCategorys = menuCategorys.map(function( menuCategory, index ){
+            menuCategory.weight = weightList.indexOf(menuCategory.sn);
+            if(menuCategory.sn in statusList){
+                menuCategory.status = true;
+            } else {
+                menuCategory.status = false;
+            }
+            return menuCategory;
+        })
+        menuCategorys.save();
+        return res.redirect('/menuCategory');
     })
     .catch(next);
 
