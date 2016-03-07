4efed697f051adba4b11a0e3d2bb1784f493b33c
webber0928 <bird23074035@gmail.com>
2016-03-08 01:54:22 +0800

    加上 create 的 route 畫面跟 controller

--- admin/controllers/menuCategory/page.create.js
+++ admin/controllers/menuCategory/page.create.js
@@ -1,25 +1,11 @@
-
 const debug = require('debug')('NOWvote:admin:controllers:menu');
 
 import co from 'co';
 import models from '../../../models';
 
-module.exports = {
-    show: function(req, res, next) {
-        co(function*() {
-            let issues = yield models.issue.find().execAsync();
-            return res.render('menu-create');
-        })
-        .catch(next);
-    },
-
-    create: function(req, res, next) {
-        console.log(req.body);
-        co(function*() {
-            let issues = yield models.issue.find().execAsync();
-            var items = [{ title: "foo", id: 1 }, { title: "bar", id: 2}, { title: "cool", id: 3 }];
-            return res.json({items: items});
-        })
-        .catch(next);
-    }
+module.exports = function(req, res, next) {
+    co(function*() {
+        return res.render('menuCategory/create');
+    })
+    .catch(next);
 }
