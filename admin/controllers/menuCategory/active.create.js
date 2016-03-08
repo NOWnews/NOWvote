29d4d7046dc4aa813b64615534bf23b00b371fdd
webber0928 <bird23074035@gmail.com>
2016-03-08 16:12:13 +0800

    create menuCategory 的韓式

---
+++ admin/controllers/menuCategory/active.create.js
@@ -0,0 +1,28 @@
+const debug = require('debug')('NOWvote:admin:controllers:menuCategory:creat');
+
+import co from 'co';
+import models from '../../../models';
+
+module.exports = function(req, res, next) {
+
+    let data = req.body;
+
+    co(function*() {
+
+        let status = data.status ? true : false;
+        let startTime = new Date( data['start-day'] + ' ' + data['start-hour'] );
+        let endTime = new Date( data['end-day'] + ' ' + data['end-hour'] );
+        let newMenuCategory = yield models.menuCategory.createAsync({
+            title: data.title,
+            desc: data.desc,
+            url: data.url,
+            startTime: startTime,
+            endTime: endTime,
+            status: status,
+            weight: 0
+        });
+
+        return res.redirect('/menuCategory');
+    })
+    .catch(next);
+}
