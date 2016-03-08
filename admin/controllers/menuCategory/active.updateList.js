ef5415b44fdfcca2822c68b6d7fcc6560bef1881
webber0928 <bird23074035@gmail.com>
2016-03-08 17:17:44 +0800

    調整 code

--- admin/controllers/menuCategory/active.updateList.js
+++ admin/controllers/menuCategory/active.updateList.js
@@ -1,5 +1,5 @@
 
-const debug = require('debug')('NOWvote:admin:controllers:menuCategory:updateList');
+const debug = require('debug')('NOWvote:admin:controllers:menuCategory:active.updateList');
 
 import co from 'co';
 import Promise from 'bluebird';
@@ -10,27 +10,19 @@ module.exports = function(req, res, next) {
 
     let data = req.body;
 
-    debug('data = %j', data);
-
     co(function*() {
-        // let data = req.body;
         let weightList = req.body.weightList.split(',');
-        let statusList = req.body['status[]'];
+        let statusList = _.isArray(req.body['status[]']) ? req.body['status[]'] : [req.body['status[]']];
 
         let menuCategories = yield models.menuCategory.find()
             .where('trashed').equals(false)
             .execAsync();
 
-        debug('menuCategories = %j', menuCategories);
-
-        debug('statusList = %j', statusList);
         let updatedMenuList = yield Promise.map(menuCategories, function(menuItem) {
-            debug('sn = %j', menuItem.sn);
 
             menuItem.weight = _.indexOf(weightList, menuItem.sn + '');
 
             if(_.indexOf(statusList, menuItem.sn + '') !== -1){
-                debug('menuItem = %j', menuItem);
                 menuItem.set('status', true);
                 return menuItem.saveAsync();
             }else{
