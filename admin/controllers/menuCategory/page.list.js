7fb635aecb3823893a4453a6e1482d7390a03443
webber0928 <bird23074035@gmail.com>
2016-03-09 10:21:20 +0800

    調整 find 的 語法

--- admin/controllers/menuCategory/page.list.js
+++ admin/controllers/menuCategory/page.list.js
@@ -8,7 +8,10 @@ module.exports = function(req, res, next) {
 
     co(function*() {
 
-        let items = yield models.menuCategory.find({ trashed: false }).sort({weight: 1}).execAsync();
+        let items = yield models.menuCategory.find()
+            .where('trashed').equals(false)
+            .sort({weight: 1})
+            .execAsync();
 
         return res.render('menuCategory/list', {items: items});
     })
