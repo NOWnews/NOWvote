eb4c2af155fb716935649fa3314b076a685a9773
wayne1025 <wayne1025.tw@hotmail.com>
2016-03-10 19:02:57 +0800

    增加列表排序update功能

--- admin/controllers/carouselBanner/action.updateList.js
+++ admin/controllers/carouselBanner/action.updateList.js
@@ -11,10 +11,31 @@ module.exports = function(req, res, next) {
     let data = req.body;
     console.log(data);
     co(function*() {
+        let weightList = data.weightList.split(',');
+        let statusList = _.isArray(data['status[]']) ? data['status[]'] : [data['status[]']];
+
+        let sliderBanners = yield models.sliderBanner.find()
+            .where('trashed').equals(false)
+            .execAsync();
+
+        let updateSliderBannerList = yield Promise.map(sliderBanners, function(bannerItem) {
+            let index = _.indexOf(weightList, String(bannerItem.sn));
+
+            bannerItem.weight = index === -1 ? bannerItem.weight: index;
+
+            if(_.indexOf(statusList, String(bannerItem.sn)) !== -1){
+                bannerItem.set('status', true);
+                return bannerItem.saveAsync();
+            }else{
+                bannerItem.set('status', false);
+                return bannerItem.saveAsync();
+
+            }
+        });
+        debug('updateSliderBannerList', updateSliderBannerList);
 
 
-
-
+        return res.status(204).send();
     })
     .catch(next);
 
