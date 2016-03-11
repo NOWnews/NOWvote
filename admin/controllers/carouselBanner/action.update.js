5fbe0d252eeebc0ea5e69e9b4189874c2b5f6441
webber0928 <bird23074035@gmail.com>
2016-03-11 15:46:19 +0800

    update 如果沒有換圖的問題

--- admin/controllers/carouselBanner/action.update.js
+++ admin/controllers/carouselBanner/action.update.js
@@ -19,36 +19,9 @@ module.exports = function(req, res, next) {
         let startTime = moment( `${data.startAtDay} ${data.startAtHour}` );
         let endTime = moment( `${data.endAtDay} ${data.endAtHour}` );
 
-        debug('imgFile = %j', imgFile);
-        // 檢查 圖片資訊
-        let extName = yield libs.checkExt(imgFile);
-        let fileName = 'picture' + moment()
-            .tz('Asia/Taipei')
-            .format('YYYYMMDD-HHmmss');
-
-        let fullFileName = `${fileName}.${extName}`;
-
-        debug('extName = %s', extName);
-        debug('fileName = %s', fileName);
-        let newFileName = imageStorage + `/${fullFileName}`;
-
-
-         // 呼叫 libs.moveFile 搬移檔案
-        let movedfilePosition = yield libs.moveFile(imgFile.path, newFileName);
-
-        debug('movedfilePosition = %s', movedfilePosition);
-
-        debug('image url = %s', `${imageStorageUrl}/${fullFileName}`);
-
-        let imageUrl = `${imageStorageUrl}/${fullFileName}`;
-
-        debug('imageUrl=%s', imageUrl);
-
-
         let sliderBanner = yield models.sliderBanner.findOne()
             .where('sn').equals(sn)
             .execAsync();
-        debug('sliderBanner=%j', sliderBanner);
 
         sliderBanner.set('title', data.title);
         sliderBanner.set('url', data.url);
@@ -56,10 +29,25 @@ module.exports = function(req, res, next) {
         sliderBanner.set('startTime', startTime);
         sliderBanner.set('endTime', endTime);
         sliderBanner.set('status', status);
-        sliderBanner.set('image', imageUrl);
+
+        // 檢查 圖片資訊
+        if(imgFile){
+
+            let extName = yield libs.checkExt(imgFile);
+            let fileName = 'picture' + moment()
+                .tz('Asia/Taipei')
+                .format('YYYYMMDD-HHmmss');
+            let fullFileName = `${fileName}.${extName}`;
+            let newFileName = imageStorage + `/${fullFileName}`;
+
+             // 呼叫 libs.moveFile 搬移檔案
+            let movedfilePosition = yield libs.moveFile(imgFile.path, newFileName);
+            let imageUrl = `${imageStorageUrl}/${fullFileName}`;
+
+            sliderBanner.set('image', imageUrl);
+        }
 
         yield sliderBanner.saveAsync();
-
         return res.redirect('/carouselBanner');
     })
     .catch(next);
