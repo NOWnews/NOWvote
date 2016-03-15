26dcbf7e29c10f0f509c53ed0f227a62529405dc
wayne1025 <wayne1025.tw@hotmail.com>
2016-03-15 10:50:48 +0800

    package.json加入gulp-autoprefixer，controller加入demo頁面js，加入main.css測試，新增controllers/home/index.js檔案，加入前端測試頁面demo.html

--- server/controllers/home/index.js
+++ server/controllers/home/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 let router = express.Router();
 
 import pageHome from './page.home';
+import pageDemo from './page.demo';
 
 router.route('/')
     .get(pageHome);
@@ -10,4 +11,6 @@ router.route('/')
 router.route('/index')
     .get(pageHome);
 
+router.route('/demo')
+    .get(pageDemo);
 module.exports = router;
---
+++ server/controllers/home/page.demo.js
@@ -0,0 +1,40 @@
+
+const debug = require('debug')('NOWvote:server:controllers:home');
+
+import co from 'co';
+import models from '../../../models';
+import redis from '../../../caches';
+
+module.exports = function(req, res, next) {
+
+    co(function*() {
+
+        // 非同步去取得資料
+        let results = yield [
+            // 從 redis 取得 menuCategory 的資料
+            yield redis.getMenuCategory(),
+
+            // 從 redis 取得 slideBanner 的資料
+            yield redis.getSliderBanner(),
+
+            // 取得首頁 issue 的資料
+            yield models.issue.find().execAsync()
+        ];
+        debug('results = %j', results);
+
+        let menuCategory = results[0];
+        let sliderBanner = results[1];
+        let issues = results[2];
+        debug('menuCategory = %j', menuCategory);
+        debug('sliderBanner = %j', sliderBanner);
+        debug('issues = %j', issues);
+
+        return res.render('demo', {
+            issues: issues,
+            menuCategory: menuCategory,
+            sliderBanner: sliderBanner
+        });
+    })
+    .catch(next);
+
+};
