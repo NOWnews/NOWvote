4cbcd522bde462e54ced3118df771b34fc808c41
WebberWu <bird23074035@gmail.com>
2016-03-23 16:54:43 +0800

    把tags 補上

--- admin/controllers/issue/action.create.js
+++ admin/controllers/issue/action.create.js
@@ -93,7 +93,7 @@ module.exports = function(req, res, next) {
             endTime: endTime,
             continued: continued,
             questions: [],
-            tags: []
+            tags: data.tages
         };
 
         let voteCounterData = {
@@ -117,7 +117,7 @@ module.exports = function(req, res, next) {
                     let newOptionId = mongoose.Types.ObjectId();
                     optionIds.push(newOptionId);
 
-                    // 產生新的 option object 推入陣列，等等要一次存進 option 
+                    // 產生新的 option object 推入陣列，等等要一次存進 option
                     let newOption = {};
                     newOption._id = newOptionId;
                     newOption.content = option;
@@ -129,7 +129,7 @@ module.exports = function(req, res, next) {
             let newQuestionId = mongoose.Types.ObjectId();
             newIssue.questions.push(newQuestionId);
 
-            // 產生新的 question 推入陣列，等等要一次存進 question 
+            // 產生新的 question 推入陣列，等等要一次存進 question
             let newQuestion = {};
             newQuestion._id = newQuestionId;
             newQuestion.content = question.name;
