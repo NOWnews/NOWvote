4d901a6d71430cb7145328488375610dba726c96
WebberWu <bird23074035@gmail.com>
2016-03-23 14:24:46 +0800

    add update issue list status

--- admin/controllers/issue/index.js
+++ admin/controllers/issue/index.js
@@ -4,14 +4,14 @@ let router = express.Router();
 import pageList from './page.list';
 import pageCreate from './page.create';
 // import pageUpdate from './page.update';
-// import actionUpdateList from './action.updateList';
+import actionUpdateList from './action.updateList';
 // import actionRemove from './action.remove';
 // import actionUpdate from './action.update';
 import actionCreate from './action.create';
 
 router.route('/')
-    .get(pageList);
-//     .put(actionUpdateList);
+    .get(pageList)
+    .put(actionUpdateList);
 //
 // router.route('/:sn')
 //     .delete(actionRemove);
