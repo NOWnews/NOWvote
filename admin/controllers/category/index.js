4ec334601718abfdc201f3aa6bb0a2bf8d775a99
WebberWu <bird23074035@gmail.com>
2016-03-23 19:12:13 +0800

    menuCategory 資料夾改成 category

--- admin/controllers/category/index.js
+++ admin/controllers/category/index.js
@@ -1,9 +1,27 @@
 import express from 'express';
 let router = express.Router();
 
-import pageCategory from './page.category';
+import pageList from './page.list';
+import pageCreate from './page.create';
+import pageUpdate from './page.update';
+import actionUpdateList from './action.updateList';
+import actionRemove from './action.remove';
+import actionUpdate from './action.update';
+import actionCreate from './action.create';
 
 router.route('/')
-    .get(pageCategory);
+    .get(pageList)
+    .put(actionUpdateList);
+
+router.route('/:sn')
+    .delete(actionRemove);
+
+router.route('/create')
+    .get(pageCreate)
+    .post(actionCreate);
+
+router.route('/update/:sn')
+    .put(actionUpdate)
+    .get(pageUpdate);
 
 module.exports = router;
