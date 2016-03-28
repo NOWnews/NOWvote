009baae06a49ff28fbb7442fc5972f9d217aa48e
WebberWu <bird23074035@gmail.com>
2016-03-28 16:48:07 +0800

    issue 時間格式 format, 並加上顏色標示走期

--- admin/controllers/issue/page.list.js
+++ admin/controllers/issue/page.list.js
@@ -1,22 +1,44 @@
 import co from 'co';
 import models from '../../../models';
+import moment from 'moment-timezone';
 
 const debug = require('debug')('NOWvote:admin:controllers:issue:page.list');
+const formatUpdateIssuesData = function (issues) {
+    let newTime = moment().tz('Asia/Taipei').valueOf();
+    debug('newTime = %j', moment().valueOf());
+    issues = _.map(issues, function (issue){
+        if (issue.continued){
+            issue.schedule = true;
+            return issue;
+        }
+
+        let startTime = moment(issue.startTime).valueOf();
+        let endTime = moment(issue.endTime).valueOf();
+
+        if (newTime > startTime && newTime < endTime) {
+            issue.schedule = true;
+        } else {
+            issue.schedule = false;
+        }
+        issue.startTime = moment(issue.startTime).tz('Asia/Taipei').format('YYYY-MM-DD HH:mm');
+        return issue;
+    });
+
+    return issues;
+};
 
 module.exports = function(req, res, next) {
 
     co(function*() {
 
-        // let items = yield models.issue.find()
-        //     .where('trashed').equals(false)
-        //     .sort({weight: 1})
-        //     .execAsync();
-
         let issues = yield models.issue.find()
-            // .populate('questions')
+            .populate('category')
             .where('trashed').equals(false)
             .deepPopulate('questions.options')
+            .lean()
             .execAsync();
+
+        issues = formatUpdateIssuesData(issues);
 
         debug('issues = %j', issues);
 
