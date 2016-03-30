eea42953f847b2d5389c8ae173c95e308aa4f05a
WebberWu <bird23074035@gmail.com>
2016-03-30 16:42:35 +0800

    第一層 route 拉到最外層的 index.js 來設定

--- server/controllers/login/index.js
+++ server/controllers/login/index.js
@@ -1,12 +1,11 @@
 
-
 import express from 'express';
 let router = express.Router();
 
 const pageLogin = require('./page.login');
 const actionLogin = require('./action.login');
 
-router.route('/login')
+router.route('/')
     .get(pageLogin)
     .post(actionLogin);
 
