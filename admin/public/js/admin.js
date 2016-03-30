f801fc2e49b6fc87c4222bc410ad251488783124
WebberWu <bird23074035@gmail.com>
2016-03-30 16:11:02 +0800

    jquery ui 太肥拿掉，改用 html.sortable

--- admin/public/js/admin.js
+++ admin/public/js/admin.js
@@ -19,62 +19,12 @@ $(function() {
             }
         });
     });
+
+    $('table .switch-input').change(function() {
+        $('#save-btn').removeClass('hide');
+    });
+
     // Menu -----------------------
-    // :: List
-    // jquery-ui Sortable
-    if ($('.column').length !== 0) {
-        var recordWeight = function(weightList) {
-            $('input[name="weightList"]').val(weightList);
-        };
-
-        $('.column').sortable({
-            connectWith: '.column',
-            handle: '.portlet-header',
-            cancel: '.portlet-toggle',
-            placeholder: 'portlet-placeholder ui-corner-all',
-            stop: function(event, ui) {
-                var sortedWeight = $('.column').sortable('toArray', {
-                    attribute: 'item-sn'
-                });
-                $('#save-btn').removeClass('hide');
-                recordWeight(sortedWeight);
-            }
-        });
-
-        $('.portlet').addClass('ui-widget ui-widget-content ui-helper-clearfix ui-corner-all').find('.portlet-header').addClass('ui-widget-header ui-corner-all').prepend('<span class="ui-icon ui-icon-minusthick portlet-toggle"></span>');
-        $('.portlet-toggle').click(function() {
-            var icon = $(this);
-            icon.toggleClass('ui-icon-minusthick ui-icon-plusthick');
-            icon.closest('.portlet').find('.portlet-content').toggle();
-        });
-
-        $('.ui-table .switch-input').change(function() {
-            $('#save-btn').removeClass('hide');
-        });
-
-        $('.remove-btn').on('click', function() {
-            event.preventDefault();
-            var sn = $(this).attr('item-sn');
-            var url = $(this).attr('page-route');
-            $.ajax({
-                url: url,
-                type: 'DELETE',
-                success: function(data, err) {
-                    if (data.trashed) {
-                        $('.bullet-item[item-sn=' + sn + ']').remove();
-                        $('.column').sortable('refresh');
-                        $('#deleteModal' + sn).foundation('close');
-                        var sortedWeight = $('.column').sortable('toArray', {
-                            attribute: 'item-sn'
-                        });
-                        recordWeight(sortedWeight);
-                    } else {
-                        alert('資料有誤 請重新整理！');
-                    }
-                }
-            });
-        });
-    }
 
     // datepicker
     if ($('#continued').length !== 0) {
@@ -139,6 +89,29 @@ $(function() {
 
 
     // Banner -----------------------
+    if($('.sortable').length !== 0) {
+        var recordWeight = function(weightList) {
+            $('input[name="weightList"]').val(weightList);
+        };
+
+        var sortableItemCount = $('.sortable > tr').children().length;
+
+        $('.sortable').sortable({
+            forcePlaceholderSize: true,
+            items: 'tr',
+            placeholder: '<tr><td colspan="'+ sortableItemCount +'">&nbsp;</td></tr>'
+        }).bind('sortupdate', function() {
+
+            var sortedWeightList = [];
+            $(this).children().each(function(index, value) {
+                var sn = $(value).attr('item-sn');
+                sortedWeightList[index] = sn;
+            });
+
+            $('#save-btn').removeClass('hide');
+            recordWeight(sortedWeightList);
+        });
+    }
     // :: fileupload使用
     var fileUpload = function(targetElement) {
         var readUrl = function(input) {
