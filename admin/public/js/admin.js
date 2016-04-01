7ee6d7440b490d9e7b653e35d654c55ff91dd91a
WebberWu <bird23074035@gmail.com>
2016-04-01 10:50:05 +0800

    調整一下js

--- admin/public/js/admin.js
+++ admin/public/js/admin.js
@@ -27,27 +27,37 @@ $(function() {
     // Menu -----------------------
 
     // externalLink 外部連結
-    if($('#externalLink').attr('checked')) {
-        $('#categoryUrl').attr('readonly', false);
-    }else{
-        $('#categoryUrl').attr('readonly', true);
-        $('#categoryUrl').val('/categoty/' + $('#categoryTitle').val());
-    }
+    var checkedExternalLink = function () {
+        var externalLink = $('#externalLink');
+        var categoryUrl = $('#categoryUrl');
+        var categoryTitle = $('#categoryTitle');
+
+        if(externalLink.attr('checked')) {
+            categoryUrl.attr('readonly', false);
+        }else{
+            categoryUrl.attr('readonly', true);
+            categoryUrl.val('/categoty/' + categoryTitle.val());
+        }
+    };
+    checkedExternalLink();
 
     // 當 input name 的時候的 event
     $('#categoryTitle').keyup(function(e) {
         if(!$('#externalLink').prop('checked')){
-            $('#categoryUrl').val('/categoty/' + $('#categoryTitle').val());
+            $('#categoryUrl').val('/categoty/' + $(this).val());
         }
     });
 
     $('#externalLink').change(function() {
+        var categoryUrl = $('#categoryUrl');
+        var categoryTitle = $('#categoryTitle');
+
         if (this.checked) {
-            $('#categoryUrl').attr('readonly', false);
-            $('#categoryUrl').val('http://');
+            categoryUrl.attr('readonly', false);
+            categoryUrl.val('http://');
         }else{
-            $('#categoryUrl').attr('readonly', true);
-            $('#categoryUrl').val('/categoty/' + $('#categoryTitle').val());
+            categoryUrl.attr('readonly', true);
+            categoryUrl.val('/categoty/' + categoryTitle.val());
         }
     });
 
