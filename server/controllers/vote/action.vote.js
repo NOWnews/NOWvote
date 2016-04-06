bcecd8927a8e1c1816ff5459d5335943e7733a46
Simon Sun <blackghost0118@gmail.com>
2016-04-06 19:51:41 +0800

    將投票的選項資料儲存到 db 內

--- server/controllers/vote/action.vote.js
+++ server/controllers/vote/action.vote.js
@@ -1,5 +1,6 @@
 
 import co from 'co';
+import Promise from 'bluebird';
 
 const debug = require('debug')('NOWvote:server:controllers:vote:action.vote');
 const models = require('../../../models');
@@ -7,9 +8,70 @@ const models = require('../../../models');
 module.exports = function(req, res, next) {
 
     let data = req.body;
-
     debug('data = %j', data);
 
-    return res.json(data);
+    co(function*() {
 
+        let issueId = data.issueId;
+        let issueSn = data.issueSn;
+        let userId = '500000000000000000000012';
+
+        let relationData = [];
+
+        // 處理傳進來的資料
+        _.forEach(data.questions, function(question) {
+            _.forEach(question.optionIds, function(option) {
+                relationData.push({
+                    user: userId,
+                    issue: issueId,
+                    question: question.questionId,
+                    option: option
+                });
+            });
+        });
+
+        // 確認這個使用者是否投過票了
+        let isVote = yield models.issueRelation.find()
+            .where('user').equals(userId)
+            .where('issue').equals(issueId)
+            .execAsync()
+            .then(function(relations) {
+                debug('relations = %j', relations);
+                if(relations.length > 0) {
+                    return Promise.resolve(true);
+                }
+                return Promise.resolve(false);
+            });
+
+        if(isVote === true) {
+            return Promise.reject(new Error('已經投票過了'));
+        }
+
+        // 這段 code 是逐一確認每個 options 與使用者個關係...
+        // yield Promise.each(relationData, function(data) {
+        //     return models.issueRelation.findOne()
+        //         .where('user').equals(data.user)
+        //         .where('issue').equals(data.issue)
+        //         .where('question').equals(data.question)
+        //         .where('option').equals(data.option)
+        //         .execAsync()
+        //         .then(function(aliveRelation) {
+        //             debug('aliveRelation = %j', aliveRelation);
+        //             if(aliveRelation) {
+        //                 return Promise.reject(new Error('已經投票過了'));
+        //             }
+
+        //             return Promise.resolve();
+        //         });
+        // });
+
+        debug('relationData = %j', relationData);
+
+        yield models.issueRelation.createAsync(relationData);
+
+        return res.json(relationData);
+
+        // return res.redirect(`/issues/${issueSn}`);
+    })
+    .catch(next);
 };
