c93f9a89fc3ca5a633d27d6e8982e22084eab44c
WebberWu <bird23074035@gmail.com>
2016-04-06 17:35:45 +0800

    vote 傳到後端 ajax

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -6,4 +6,33 @@ $(function() {
     }).on('sticky.zf.unstuckfrom:top', function() {
         $(this).removeClass('shrink');
     });
+
+    // vote
+    $('#vote-btn').on('click', function() {
+        var voteFormDatas = $('.vote-form').serializeArray();
+        var url = $('.vote-form').attr('action');
+        var issueId = $('.vote-form').attr('issue-id');
+        var questions = _.map(voteFormDatas, function(date) {
+            var optionIds = _.isArray(date.value) ? date.value : [date.value];
+            var question = {
+                questionId: date.name,
+                optionIds: optionIds
+            };
+            return question;
+        });
+        var voteData = {
+            issueId: issueId,
+            questions: questions
+        };
+        $.ajax({
+            url: url,
+            type: 'POST',
+            dataType: 'json',
+            contentType: 'application/json',
+            data: JSON.stringify(voteData),
+            success: function(data, err) {
+                console.log('L33', data);
+            }
+        });
+    });
 });
