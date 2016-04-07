86e0cddeedc5868ebb2cdc4169ebf4fe630c7bc9
wayne1025 <wayne1025.tw@hotmail.com>
2016-04-07 12:46:34 +0800

    前台調整 news區塊樣式

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -40,4 +40,11 @@ $(function() {
             }
         });
     });
+
+    //大三小六
+    $('.tabs').on('change.zf.tabs', function(e, tgt){
+         var tabchange= $('.is-active.tabs-title').find('a').attr('aria-controls');
+        $('#'+tabchange+'>div').foundation('destroy');
+        $('#'+tabchange+'>div').foundation();
+    });
 });
