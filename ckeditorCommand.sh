03a295f9269ac82c3a37f9aa88facce2001ddd62
Simon Sun <blackghost0118@gmail.com>
2016-04-11 13:11:25 +0800

    修正在 ubuntu 上面的錯誤

--- ckeditorCommand.sh
+++ ckeditorCommand.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed -i "" "s/config.removeDialogTabs\ =\ 'image:advanced;link:advanced';/config.removeDialogTabs='image:advanced;link:advanced';config.image_previewText='\ \ ';config.filebrowserImageUploadUrl='\/ckeditor\/fileUpload';/" ./bower_components/ckeditor/config.js;
+sed -i "s/config.removeDialogTabs\ =\ 'image:advanced;link:advanced';/config.removeDialogTabs='image:advanced;link:advanced';config.image_previewText='\ \ ';config.filebrowserImageUploadUrl='\/ckeditor\/fileUpload';/" ./bower_components/ckeditor/config.js;
