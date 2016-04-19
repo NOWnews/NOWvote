7b5582a2b5f05cdad3cc0cf139e60aea07a0f65d
WebberWu <bird23074035@gmail.com>
2016-04-19 17:51:00 +0800

    user router

--- server/controllers/user/page.meIssues.js
+++ server/controllers/user/page.meIssues.js
@@ -2,5 +2,5 @@
 const debug = require('debug')('NOWvote:server:controllers:user:page.myIssues');
 
 module.exports = function(req, res, next) {
-
+    return res.render('user/meIssues');
 };
--- server/controllers/user/page.show.js
+++ server/controllers/user/page.show.js
@@ -2,5 +2,5 @@
 const debug = require('debug')('NOWvote:server:controllers:user:page.show');
 
 module.exports = function(req, res, next) {
-
+    return res.render('user/show');
 };
