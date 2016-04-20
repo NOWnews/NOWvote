e508d67f79e7d00b7faaede8ce53ca31c2053596
Simon Sun <blackghost0118@gmail.com>
2016-04-20 16:42:46 +0800

    可惡的 gitlab

--- libs/errorWrapper.js
+++ libs/errorWrapper.js
@@ -5,7 +5,7 @@
  * code: 自定義的錯誤碼 
  * name: 自定義的錯誤名稱 
  * type: 輸出的型態，要 json 或是 web page 
- * errorObject: error 的物件，可以帶入或是由這邊產生 
+ * errorObject: error 的物件，可以帶入或是由這邊產生
  */
 
 module.exports = function(code, message, type, errorObject) {
