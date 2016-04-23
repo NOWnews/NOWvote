26dc653b25e9099f598f4f8ae919c6ad52e138f8
WebberWu <bird23074035@gmail.com>
2016-04-23 17:26:34 +0800

    堅立使用者回條 資料表

---
+++ models/userReceipt.js
@@ -0,0 +1,67 @@
+
+import co from 'co';
+import Promise from 'bluebird';
+import is from 'is_js';
+import mongoose from 'mongoose';
+import autoIncrement from 'mongoose-auto-increment';
+
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+
+    sn: {
+        type: Number,
+        required: true
+    },
+
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+
+    text: {
+        type: String
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+
+    trashed: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: {
+        updatedAt: 'updatedAt'
+    }
+});
+
+schema.statics.findBySn = co.wrap(function*(sn) {
+
+    let self = this;
+    let thisSn = parseInt(sn, 10);
+
+    if(!is.number(thisSn)){
+        return yield Promise.reject(new Error('sn must number'));
+    }
+
+    return yield self
+        .findOne()
+        .where('sn').equals(thisSn)
+        .execAsync();
+});
+
+schema.plugin(autoIncrement.plugin, {
+    model: 'userReceipt',
+    field: 'sn',
+    startAt: 1
+});
+
+module.exports = mongoose.model('userReceipt', schema);
