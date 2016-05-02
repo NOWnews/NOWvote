7a73d4f257c75c8ade96a9fe12b1300d34fe9ac2
WebberWu <bird23074035@gmail.com>
2016-05-02 10:46:39 +0800

    暫時將 sticky 的 js 拿掉，改用 css 方式處理，將 bottom 的按鈕背景改成透明的

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -210,7 +210,7 @@ $(function() {
         });
     });
 
-    $('.header').sticky({topSpacing:0});
+    // $('.header').sticky({topSpacing:0});
     $('.mask').on('click', function(){
         $('.hamburger').click();
     });
